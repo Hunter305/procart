4f5218f240bf83aecedeb15c8cd266113150b813
Hunter305 <avinash8169@gmail.com>
2023-07-09 12:04:30 +0530

    added cart functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,12 +14,14 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import { Provider } from "react-redux";
 import store from "./store";
+import CartScreen from "./screens/CartScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
     </Route>
   )
 );
