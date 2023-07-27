aec148ed1b57282196bf50044b063561168317ed
Hunter305 <avinash8169@gmail.com>
2023-07-27 12:54:47 +0530

    app completed

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,36 +1,36 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const SearchBox = () => {
   const navigate = useNavigate();
   const { keyword: urlKeyword } = useParams();
 
   // FIX: uncontrolled input - urlKeyword may be undefined
-  const [keyword, setKeyword] = useState(urlKeyword || '');
+  const [keyword, setKeyword] = useState(urlKeyword || "");
 
   const submitHandler = (e) => {
     e.preventDefault();
     if (keyword) {
       navigate(`/search/${keyword.trim()}`);
-      setKeyword('');
+      setKeyword("");
     } else {
-      navigate('/');
+      navigate("/");
     }
   };
 
   return (
-    <Form onSubmit={submitHandler} className='d-flex'>
+    <Form onSubmit={submitHandler} className="d-flex">
       <Form.Control
-        type='text'
-        name='q'
+        type="text"
+        name="q"
         onChange={(e) => setKeyword(e.target.value)}
         value={keyword}
-        placeholder='Search Products...'
-        className='mr-sm-2 ml-sm-5'
+        placeholder="Search Products..."
+        className="mr-sm-2 ml-sm-5"
       ></Form.Control>
-      <Button type='submit' variant='outline-success' className='p-2 mx-2'>
+      <Button type="submit" variant="success" className="p-2 mx-2">
         Search
       </Button>
     </Form>
